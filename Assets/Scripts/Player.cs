dbdaf4073636bfd24214d3bfd9d7c2385998871b
Alexander Usovich <usovich@vandrouka.games>
2021-09-28 23:29:07 +0300

    Added basic collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,16 @@ namespace project
 {
     public class Player : MonoBehaviour
     {
+        private const string TOUCHABLE_TAG = "Touchable";
+        [SerializeField] private GameObject playerBody;
 
+        void OnCollisionEnter(Collision other)
+        {
+            if(other.gameObject.tag == TOUCHABLE_TAG)
+            {
+                Debug.Log("Collision");
+                
+            }
+        }
     }
 }
