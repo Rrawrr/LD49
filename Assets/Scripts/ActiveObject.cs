eb7858310429489c5e35d93c64ca6a6fea08289b
Alexander Usovich <usovich@vandrouka.games>
2021-09-30 22:45:17 +0300

    Added swap object mechanic

--- Assets/Scripts/ActiveObject.cs
+++ Assets/Scripts/ActiveObject.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace project
@@ -5,17 +6,38 @@ namespace project
     public class ActiveObject : MonoBehaviour
     {
         private const string TOUCHABLE_TAG = "Touchable";
+        private bool isActive = true;
+
 
         void OnCollisionEnter(Collision other)
         {
             if(other.gameObject.tag == TOUCHABLE_TAG)
             {
-                Debug.Log("Collision");
-                Controller.I.SetPlayerObject(other.gameObject);
-                other.gameObject.AddComponent<ActiveObject>();
-                Destroy(gameObject.GetComponent<ActiveObject>());
-
+                GameObject otherGO = other.gameObject;
+                ChangeActiveObject(otherGO);
             }
+        }
+
+        private void ChangeActiveObject(GameObject other)
+        {
+            Debug.Log($"Collision from {gameObject.name}");
+
+            if (isActive && other.GetComponent<ActiveObject>() == null)
+            {
+                Controller.I.SetPlayerObject(other);
+                other.GetComponent<MeshRenderer>().material.color = Color.red;
+                other.gameObject.AddComponent<ActiveObject>();
+                isActive = false;
+
+                StartCoroutine(DeactivateOldActiveGameObjectCoroutine(other));
+            }
+        }
+
+        IEnumerator DeactivateOldActiveGameObjectCoroutine(GameObject other)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.green;
+            yield return new WaitForSeconds(5);
+            Destroy(gameObject.GetComponent<ActiveObject>());
         }
     }
 }
